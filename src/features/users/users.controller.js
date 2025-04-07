f1b85cbc789d134fba009370b864af745b85041b
Harsh Mishra <kanak121@gmail.com>
2025-04-07 12:48:18 +0530

    your message

--- src/features/users/users.controller.js
+++ src/features/users/users.controller.js
@@ -6,9 +6,10 @@ class userController {
     signUp=async(req,res,next)=>{
         try{
             const {name,email,password}=req.body;
-            if(password.trim()<10){
+            if(password.trim().length < 10){
                 return res.status(400).send({success:false,message:"password should b atleast 10 characters"})
             }
+            
             const hashPassword= await bcrypt.hash(password,10);
             const user=await userRepository.signUp(name,email,hashPassword);
             return res.status(201).json({success:true,message:"User is successfully registered"})
