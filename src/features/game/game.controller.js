25f532c878316bd9c53716798a4ffd105c8f6662
Harsh Mishra <kanak121@gmail.com>
2025-04-14 01:05:24 +0530

    your message

--- src/features/game/game.controller.js
+++ src/features/game/game.controller.js
@@ -1,21 +1,22 @@
 import { GameRepository } from "./game.repository.js";
 
+// Global counter that will be shared among all connections
+let globalCounter = 1;
+
 class GameController {
-  constructor() {
-    this.value = 1;  // Global counter that increments for all users
-  }
+  constructor() {}
 
   givingTimer = (req, res, next) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');  // Make sure to allow all origins for CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Send the same value to every connected client every 1 second
+    // Broadcast the same value to all clients every 1 second
     const interval = setInterval(() => {
-      console.log('sending message to all clients: ', this.value);
-      res.write(`data: ${this.value}\n\n`);  // Send the current value to the client
-      this.value++;  // Increment the value for the next broadcast
+      console.log('sending message to all clients: ', globalCounter);
+      res.write(`data: ${globalCounter}\n\n`);
+      globalCounter++;  // Increment the global counter
     }, 1000);
 
     req.on('close', () => {
